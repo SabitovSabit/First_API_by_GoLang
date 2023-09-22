d7f6f170f585eddc8617e07e3223e5b1f6db5468
SabitSabitov <SabitNs@code.edu.az>
2023-09-22 18:39:53 +0400

    post

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import(
+import (
+	"errors"
 	"net/http"
+    
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,8 +23,46 @@ func getCars(c *gin.Context){
 	c.IndentedJSON(http.StatusOK,cars)
 }
 
+func carById(c *gin.Context){
+	id:=c.Param("id")
+
+	scar,err:=getCarById(id)
+
+	if err!=nil{
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"not found!"})
+		return 
+	}
+
+	c.IndentedJSON(http.StatusOK,scar)
+}
+func getCarById(id string) (*car,error){
+	
+	for i,item :=range cars{
+		 if item.ID==id{
+			return  &cars[i],nil
+		 }
+	}
+
+	return nil,errors.New("car not found")
+}
+
+func createCar(c *gin.Context){
+
+	var newCar car
+
+	if err:=c.BindJSON(&newCar);err!=nil{
+		return
+	}
+
+	cars=append(cars,newCar)
+	c.IndentedJSON(http.StatusCreated,newCar)
+}
+
+
 func main(){
 	router:=gin.Default()
+	router.POST("/cars",createCar)
+	router.GET("/cars/:id",carById)
 	router.GET("/cars",getCars)
 	router.Run("localhost:8080")
 }
